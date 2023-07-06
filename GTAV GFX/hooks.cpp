9e3d724cafd6e26fbd35f609f13e90a153d2b5e7
rfoodxmodz <64507894+rfoodxmodz@users.noreply.github.com>
2023-07-06 22:43:09 +0300

    Add files via upload

--- GTAV GFX/hooks.cpp
+++ GTAV GFX/hooks.cpp
@@ -11,7 +11,7 @@ NAMESPACE(Hooks)
 gfxLoader_t gfxLoader_Stub;
 
 uint64_t gfxLoader_hook(long long a1, const char *path, long long a3, unsigned int a4, unsigned int a5) {
-	if(!strcmp(path, "font_lib_efigs_ps4.gfx")) {
+	if(strstr(path, "font_lib_efigs")) {
 		sceSysUtilSendSystemNotificationWithText(222, "font_lib_efigs_ps4.gfx Loaded.");
 		return gfxLoader_Stub(a1, "/data/font_lib_efigs_ps4.gfx", a3, a4, a5);
 	}
