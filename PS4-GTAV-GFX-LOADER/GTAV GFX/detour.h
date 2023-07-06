5b7700f4fdec5c73e015314e4dee832da0230cbf
rfoodxmodz <64507894+rfoodxmodz@users.noreply.github.com>
2023-07-06 21:48:42 +0300

    Delete PS4-GTAV-GFX-LOADER directory

--- PS4-GTAV-GFX-LOADER/GTAV GFX/detour.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "types.h"
-
-#define HOOK_LENGTH 14
-
-void PatchInJump(uint64_t address, void *destination);
-void *DetourFunction(uint64_t address, void *destination, int length);
